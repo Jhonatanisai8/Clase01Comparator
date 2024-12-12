2b721de719ff1852dbfeaa6f74f070c6b4ed81e2
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-11 22:42:30 -0500

    ejercicio de entrada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new App();
     }
+
+    public App() {
+        // crear la lista de enteros
+        List<Integer> listaNumeros = crearLista();
+        listaNumeros.forEach(System.out::println);
+        System.out.println("===");
+        // quedarme con solo los pares
+        List<Integer> listaPares = soloPares(listaNumeros);
+        // mostrar
+        listaPares.forEach(System.out::println);
+        // pasar cada numero al cuadrado
+        List<Integer> listaNumerosCuadros = pasarListaCuadrado(listaPares);
+        // mostramos
+        System.out.println("CUADRADO");
+        listaNumerosCuadros.forEach(System.out::println);
+        // obtenemos la suma de los cuadros
+        int suma = obtenerSumaCuadrados(listaNumerosCuadros);
+        System.out.println("SUMA DE LOS CUADRADOS: "+suma);
+    }
+
+    private int obtenerSumaCuadrados(List<Integer> listaNumerosCuadros) {
+        int suma = 0;
+        for (int i = 0; i < listaNumerosCuadros.size(); i++) {
+            suma += listaNumerosCuadros.get(i);
+        }
+        return suma;
+    }
+
+    private List<Integer> pasarListaCuadrado(List<Integer> listaPares) {
+        int resultadoCuadrado;
+        List<Integer> lista = new ArrayList<>();
+        for (int i = 0; i < listaPares.size(); i++) {
+            resultadoCuadrado = (int) Math.pow(listaPares.get(i), 2);
+            lista.add(resultadoCuadrado);
+        }
+        return lista;
+    }
+
+    private List<Integer> soloPares(List<Integer> listaNumeros) {
+        for (int i = 0; i < listaNumeros.size(); i++) {
+            if (listaNumeros.get(i) / 2 != 0) {
+                listaNumeros.remove(i);
+            }
+        }
+        return listaNumeros;
+    }
+
+    private List<Integer> crearLista() {
+        List<Integer> listaNumeros = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int numeroGenerado = random.nextInt(10) + 1;
+            listaNumeros.add(numeroGenerado);
+        }
+        return listaNumeros;
+    }
+
 }
